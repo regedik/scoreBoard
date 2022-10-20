829f0a407f69af7fc1a1be59f2ae7d9b9938b471
Evgeniy Shtuforuk <regedik22@gmail.com>
2022-10-21 01:08:52 +0300

    Final release

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let homeScore = 0;
-let guestScore = 0;
+var homeScore = 0;
+var guestScore = 0;
 
 let homeScoreComp = document.getElementById('home_score');
 let guestScoreComp = document.getElementById('guest_score');
@@ -12,16 +12,22 @@ let homeTitle = document.getElementById('home_title');
 //let timeTitle = document.getElementById('time_title');
 let guestTitle = document.getElementById('guest_title');
 
-let homePlus1Btn = document.getElementById('home_plus1_btn');
-let homePlus2Btn = document.getElementById('home_plus2_btn');
-let homePlus3Btn = document.getElementById('home_plus3_btn');
-let guestPlus1Btn = document.getElementById('guest_plus1_btn');
-let guestPlus2Btn = document.getElementById('guest_plus2_btn');
-let guestPlus3Btn = document.getElementById('guest_plus3_btn');
+let homePlus1Btn = document.getElementById('home_plus_1_btn');
+let homePlus2Btn = document.getElementById('home_plus_2_btn');
+let homePlus3Btn = document.getElementById('home_plus_3_btn');
+let guestPlus1Btn = document.getElementById('guest_plus_1_btn');
+let guestPlus2Btn = document.getElementById('guest_plus_2_btn');
+let guestPlus3Btn = document.getElementById('guest_plus_3_btn');
+let newgameBtn = document.getElementById('newgame_btn');
 
 const gameTimeMax = 10;
 let gameTime = gameTimeMax;
-timeComp.textContent = gameTime;
+
+initiate();
+
+function initiate() {
+    disablePlusButtons(true);
+}
 
 function setTimer() {
     setTimeout(() => {
@@ -37,25 +43,23 @@ function timerHandler() {
         setTimer();
     }
     else {
-        homePlus1Btn.disabled = true;
-        homePlus2Btn.disabled = true;
-        homePlus3Btn.disabled = true;
-        guestPlus1Btn.disabled = true;
-        guestPlus2Btn.disabled = true;
-        guestPlus3Btn.disabled = true;
-        
-        if(homeScore > guestScore) {
-            homeTitle.classList.add("win");
-            guestTitle.classList.add("lost");
-        }
-        else if(homeScore < guestScore) {
-            homeTitle.classList.add("lost");
-            guestTitle.classList.add("win");
-        }
-        else {
-            homeTitle.classList.add("draw");
-            guestTitle.classList.add("draw");
-        }
+        disablePlusButtons(true);
+        highlightTitles();
+    }
+}
+
+function highlightTitles() {
+    if(homeScore > guestScore) {
+        homeTitle.classList.add("win");
+        guestTitle.classList.add("lost");
+    }
+    else if(homeScore < guestScore) {
+        homeTitle.classList.add("lost");
+        guestTitle.classList.add("win");
+    }
+    else {
+        homeTitle.classList.add("draw");
+        guestTitle.classList.add("draw");
     }
 }
 
@@ -64,57 +68,45 @@ function updateScores() {
     guestScoreComp.textContent = guestScore;
 }
 
-function homePlus1Score() {
-    homeScore += 1;
+function buttonClickHandler(event) {
+    let buttonId = this.event.srcElement.id;
+    if('newgame_btn' === buttonId) {
+        newgame();
+        return;
+    }
+    
+    const [title, ,inc] = buttonId.split("_");
+    let varName = title+'Score';
+    window[title+'Score'] += Number(inc);
     updateScores();
 }
 
-function homePlus2Score() {
-    homeScore += 2;
-    updateScores()
-}
-
-function homePlus3Score() {
-    homeScore += 3;
-    updateScores()
-}
-
-function guestPlus1Score() {
-    guestScore += 1;
-    updateScores()
-}
-
-function guestPlus2Score() {
-    guestScore += 2;
-    updateScores()
-}
-
-function guestPlus3Score() {
-    guestScore += 3;
-    updateScores()
-}
-
-function newgame() {
+function newgame() {    
     gameTime = gameTimeMax;
-    
+    timeComp.textContent = gameTime;
     homeScore = 0;
     guestScore = 0;
     updateScores()
-    
-    homeTitle.classList.remove("win");
-            homeTitle.classList.remove("lost");
-            homeTitle.classList.remove("draw");
-            guestTitle.classList.remove("win");
-            guestTitle.classList.remove("lost");
-            guestTitle.classList.remove("draw");
-    
-    homePlus1Btn.disabled = false;
-        homePlus2Btn.disabled = false;
-        homePlus3Btn.disabled = false;
-        guestPlus1Btn.disabled = false;
-        guestPlus2Btn.disabled = false;
-        guestPlus3Btn.disabled = false;
-        
-        timeComp.textContent = gameTime;
+    resetTitles();
+    disablePlusButtons(false);
     setTimer();
+}
+
+function resetTitles() {
+    homeTitle.classList.remove("win");
+    homeTitle.classList.remove("lost");
+    homeTitle.classList.remove("draw");
+    guestTitle.classList.remove("win");
+    guestTitle.classList.remove("lost");
+    guestTitle.classList.remove("draw");
+}
+
+function disablePlusButtons(state) {
+    homePlus1Btn.disabled = state;
+    homePlus2Btn.disabled = state;
+    homePlus3Btn.disabled = state;
+    guestPlus1Btn.disabled = state;
+    guestPlus2Btn.disabled = state;
+    guestPlus3Btn.disabled = state;
+    newgameBtn.disabled = !state;
 }
